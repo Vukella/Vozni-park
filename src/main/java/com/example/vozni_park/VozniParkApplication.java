6b85df98d7bfca5e3faa54a3eb65f5e2c8230da2
Vukella <nemanja.vukelic@outlook.com>
2026-01-23 10:22:50 +0100

    creates SpringBoot project with needed dependencies

---
+++ src/main/java/com/example/vozni_park/VozniParkApplication.java
@@ -0,0 +1,13 @@
+package com.example.vozni_park;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VozniParkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VozniParkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/vozni_park/VozniParkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vozni_park;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VozniParkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
